85f38f2b2633a6e0035793facb6cdfeadcfd8e45
Bigboss2Ez <141292282+Bigboss13579@users.noreply.github.com>
2025-06-02 22:09:09 +0700

    edit deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: 'fit-fusion-forum-hub',
   server: {
     host: "::",
     port: 8080,
